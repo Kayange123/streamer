784baa61722edd22670a81dcb41e99e9e14d571c
Ayubu Kayange <kayangejr3@gmail.com>
2024-01-30 17:33:54 +0300

    implement side bar

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Wrapper from "../common/Wrapper";
 import Toggle from "../common/Toggle";
+import Recommended from "./Recommended";
+import { getRecommended } from "@/lib/recommended-service";
 
-const SideBar = () => {
+const SideBar = async () => {
+  const recommendedUsers = await getRecommended();
   return (
     <Wrapper>
       <Toggle />
+      <div className="pt-4 lg:pt-0 space-y-4">
+        <Recommended data={recommendedUsers} />
+      </div>
     </Wrapper>
   );
 };
