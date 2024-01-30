9329759926b138e1f8ecea717696f41f4b822afc
Ayubu Kayange <kayangejr3@gmail.com>
2024-01-30 13:37:52 +0300

    Update Toggle.tsx

--- components/common/Toggle.tsx
+++ components/common/Toggle.tsx
@@ -4,28 +4,33 @@ import { useSidebar } from "@/hooks/use-sidebar";
 import React from "react";
 import { Button } from "../ui/button";
 import { ArrowLeftFromLineIcon, ArrowRightFromLineIcon } from "lucide-react";
+import Hint from "../shared/hint";
 
 const Toggle = () => {
   const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
   const label = collapsed ? "Expand" : "Collapse";
   return (
     <>
-      {!collapsed ? (
+      {collapsed ? (
         <div className="hidden lg:flex w-full items-center justify-center py-4">
-          <Button variant="ghost" className="h-auto p-2" onClick={onExpand}>
-            <ArrowRightFromLineIcon className="h-4 w-4" />
-          </Button>
+          <Hint label={label} side="right" asChild>
+            <Button variant="ghost" className="h-auto p-2" onClick={onExpand}>
+              <ArrowRightFromLineIcon className="h-4 w-4" />
+            </Button>
+          </Hint>
         </div>
       ) : (
         <div className="p-3 pl-6 mb-2 flex items-center w-full">
           <p className="font-semibold text-primary">For you</p>
-          <Button
-            onClick={onCollapse}
-            variant="ghost"
-            className="h-auto ml-auto p-2"
-          >
-            <ArrowLeftFromLineIcon className="h-4 w-4" />
-          </Button>
+          <Hint label={label} side="right" asChild>
+            <Button
+              onClick={onCollapse}
+              variant="ghost"
+              className="h-auto ml-auto p-2"
+            >
+              <ArrowLeftFromLineIcon className="h-4 w-4" />
+            </Button>
+          </Hint>
         </div>
       )}
     </>
