4df804d2dc94855ab2fcd674ed1a973a1e6de06c
Ayubu Kayange <kayangejr3@gmail.com>
2024-02-03 00:01:29 +0300

    update error boundary page

--- app/(root)/[username]/error.tsx
+++ app/(root)/[username]/error.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
 import React from "react";
 
-const UserErrorPage = () => {
-  return <div>UserErrorPage</div>;
+interface UserErrorPageProps {
+  error: Error & {
+    digest?: string;
+  };
+  reset: () => void;
+}
+
+const UserErrorPage = ({ error, reset }: UserErrorPageProps) => {
+  return (
+    <div className="w-full h-full flex items-center justify-center space-y-5">
+      <p className="">Something went wrong while loading this page!</p>
+      <Button asChild onClick={() => reset()}>
+        <RefreshCcw className="w-4 h-4" />
+        <p className="">Retry</p>
+      </Button>
+    </div>
+  );
 };
 
 export default UserErrorPage;
